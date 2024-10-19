6c5012323e26db88851c67a39dd6751b581a1803
B Tasker <github@bentasker.co.uk>
2024-10-19 11:37:12 +0100

    feat: check date of jobs and process those which need it

--- app/scheduled_toots.py
+++ app/scheduled_toots.py
@@ -6,7 +6,49 @@
 
 
 import os
+import sys
 from datetime import datetime as dt
+
+
+def triggerJobs(jobs, oldjob_dir, runtime):
+    ''' Iterate through jobs checking time against now
+    
+    Trigger those which need triggering
+    '''
+    
+    today = runtime.strftime("%Y-%m-%d")
+    today_dir = f"{oldjob_dir}/{today}"
+    
+    for job in jobs:
+        if not job["publish_at"]:
+            # job was scheduled without a time, flag but ignore
+            # TODO: should we have an "errored" dir in the heirachy?
+            print(f"Err: Job {job['fname']} has no parseable time", file=sys.stderr)
+            continue
+        
+        # Parse the date
+        try:
+                pub_date = dt.strptime(job["publish_at"], '%Y-%m-%dT%H:%M:%S%Z').astimezone()
+        except Exception as e:
+            print(f"Err: Job {job['fname']} time failed to parse {e}", file=sys.stderr)
+            continue            
+
+        # Check if the toot is due
+        if pub_date <= runtime:
+            # RUN!
+            try:
+                print("Send toot")
+            except Exception as e:
+                print(f"Err: Job {job['fname']} failed to run correctly {e}", file=sys.stderr)
+
+            # Either way, move the file out of the way
+            if not os.path.exists(today_dir):
+                os.makedirs(today_dir)
+            
+            fname = os.path.basename(job["fname"])
+            os.rename(job["fname"], f"{today_dir}/{fname}")
+            
+
 
 
 def loadJobs(newjob_dir):
@@ -15,6 +57,9 @@ def loadJobs(newjob_dir):
     '''
     jobs = []
     for f in os.listdir(newjob_dir):
+        if f.endswith(".swp"):
+            # Some muppet left nano open
+            continue
         jobs.append(loadJobFile(f"{newjob_dir}/{f}"))
 
     return jobs
@@ -25,7 +70,8 @@ def loadJobFile(file_path):
     '''
     fh = open(file_path, 'r')
     f = {
-        "publish_at" : False
+        "publish_at" : False,
+        "fname" : file_path
         }
     
     text_buffer = []
@@ -60,11 +106,15 @@ def loadJobFile(file_path):
     # in join
     f["text"] = "".join(text_buffer).lstrip("\n")
     
+    if f["publish_at"].endswith("Z"):
+        f["publish_at"] = f["publish_at"].replace("Z", "UTC")
+    
     return f
 
 
 if __name__ == "__main__":
     job_dir = os.getenv("JOB_DIR", "/jobs")
+    runtime = dt.now().astimezone()
     # Check that the heirachy exists
     paths = {}
     for sub in ["new", "done"]:
@@ -74,6 +124,6 @@ if __name__ == "__main__":
             
 
     jobs = loadJobs(paths["new"])
-    
+    triggerJobs(jobs, paths["done"], runtime)
 
             
